bb2c516e6aab95aed8e2f74ab10bad1642d3099d
Hu Chunxu <huchunxu@guyuehome.com>
2022-03-16 08:13:29 +0800

    添加一个简单的launch示例

---
+++ learning_launch/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'learning_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hcx',
+    maintainer_email='huchunxu@guyuehome.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
