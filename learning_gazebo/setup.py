661fbb04f64098478a680bea634c5b5c84646122
Hu Chunxu <huchunxu@ps-micro.com>
2022-04-27 16:07:40 +0800

    解决gazebo包中的配置问题

--- learning_gazebo/setup.py
+++ learning_gazebo/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
         (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.*'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.*'))),
         (os.path.join('share', package_name, 'urdf/sensors'), glob(os.path.join('urdf/sensors', '*.*'))),
         (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.*'))),
         (os.path.join('share', package_name, 'models/meshes'), glob(os.path.join('models/meshes', '*.*'))),    
