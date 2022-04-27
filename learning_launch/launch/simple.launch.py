df17759fe45589bd007a8d6d6e51f87d24c26464
huchunxu <huchunxu@ps-micro.com>
2022-04-28 01:23:30 +0800

    优化launch文件

--- learning_launch/launch/simple.launch.py
+++ learning_launch/launch/simple.launch.py
@@ -1,10 +1,18 @@
 from launch import LaunchDescription
-import launch_ros.actions
+from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        launch_ros.actions.Node(
-            namespace= "turtlesim1", package='turtlesim', executable='turtlesim_node', output='screen'),
-        launch_ros.actions.Node(
-            namespace= "turtlesim2", package='turtlesim', executable='turtlesim_node', output='screen'),
+        Node(
+            namespace= "turtlesim1", 
+            package='turtlesim', 
+            executable='turtlesim_node', 
+            output='screen'
+        ),
+        Node(
+            namespace= "turtlesim2", 
+            package='turtlesim', 
+            executable='turtlesim_node', 
+            output='screen'
+        ),
     ])
