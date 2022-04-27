b9b6b9bc2ffc891d8ef5925f0fd5126815cdd31d
huchunxu <huchunxu@ps-micro.com>
2022-04-27 13:46:34 +0800

    添加gazebo仿真模型启动文件

---
+++ learning_gazebo/launch/load_mbot_rgbd_into_gazebo.launch.py
@@ -0,0 +1,60 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='learning_gazebo' #<--- CHANGE ME
+    world_file_path = 'worlds/neighborhood.world'
+    
+    pkg_path = os.path.join(get_package_share_directory(package_name))
+    world_path = os.path.join(pkg_path, world_file_path)  
+    
+    # Pose where we want to spawn the robot
+    spawn_x_val = '0.0'
+    spawn_y_val = '0.0'
+    spawn_z_val = '0.0'
+    spawn_yaw_val = '0.0'
+  
+    mbot = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','mbot_rgbd.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'world':world_path}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'mbot',
+                                   '-x', spawn_x_val,
+                                   '-y', spawn_y_val,
+                                   '-z', spawn_z_val,
+                                   '-Y', spawn_yaw_val],
+                        output='screen')
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        mbot,
+        gazebo,
+        spawn_entity,
+    ])
