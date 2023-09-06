92306be58a3c12c41d00d6e23b974b3db8d2f8b9
Hu Chunxu <huchunxu@ps-micro.com>
2023-09-06 23:20:55 +0800

    添加mbot的rgbd仿真模型

--- learning_gazebo/launch/mbot_rgbd.launch.py
+++ learning_gazebo/launch/mbot_rgbd.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('learning_gazebo'))
-    xacro_file = os.path.join(pkg_path,'urdf','mbot_with_kinect_gazebo.xacro')
+    xacro_file = os.path.join(pkg_path,'urdf','mbot_with_rgbd_gazebo.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
